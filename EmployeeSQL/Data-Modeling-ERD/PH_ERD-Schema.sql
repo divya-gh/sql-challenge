a26244696f978680d6f7874dafd0095a66b0c9d2
Divya Shetty <divyashettyk@gmail.com>
2021-03-05 20:42:24 -0600

    adding ERD png and schema files

---
+++ EmployeeSQL/Data-Modeling-ERD/PH_ERD-Schema.sql
@@ -0,0 +1,92 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- ERD Table was created using https://app.quickdatabasediagrams.com/
+-- Sketch out an ERD of the tables
+-- Sketch ERD for departmnet.csv
+-- dept_no is UNIQUE in the csv file , making it a 'Primary Key' to uniquely identify rows
+
+CREATE TABLE "department" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+-- Sketch ERD for employees.csv
+-- emp_no is UNIQUE, can be used as a 'Primary Key' to uniquely identify the rows
+-- emp_title_id is related to titles table
+-- Sketch ERD for titles.csv
+-- title_id is UNIQUE, can be used as a 'Primary Key' to uniquely identify the rows
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(50)   NOT NULL,
+    "title" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" VARCHAR(50)   NOT NULL,
+    "birth_date" VARCHAR(50)   NOT NULL,
+    "first_name" VARCHAR(150)   NOT NULL,
+    "last_name" VARCHAR(150)   NOT NULL,
+    "sex" VARCHAR(50)   NOT NULL,
+    "hire_date" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Sketch ERD for dept_emp.csv
+-- dept_no is related to department table
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+-- Sketch ERD for dept_manager.csv
+-- emp_no is UNIQUE again, can be used as a 'Primary Key' to uniquely identify rows
+-- emp_no is related to employees table while dept_no is related to department table
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Sketch ERD for salary.csv
+-- emp_no is UNIQUE, can be used as a 'Primary Key' to uniquely identify the rows
+-- emp_id is also related to employees table
+CREATE TABLE "salary" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salary" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salary" ADD CONSTRAINT "fk_salary_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
